e6326f489062ac0f955bd0a112b0cc808a0e0257
Jelox <monettiluca03@gmail.com>
2021-10-27 23:07:33 +0200

    Modifiche: Schermata + Navbar
    Aggiunta Home Page + Modifica animazioni Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Navbar from "./Components/Navbar.jsx";
-
+import Home from "./Pages/Home/Home.jsx";
 // Main App Function
 const App = () => {
     return (
         <div className="app">
             <Navbar />
+            <Home />
         </div>
     );
 };
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+// Import
+import React from "react";
+import "./Home.css";
+
+// Components
+const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="hero-container">
+                <div className="hero-content">
+                    <div className="text">
+                        <h1>LG House</h1>
+                        <p>The greatest innovation in the tech world</p>
+                        <a href="#" className="button button-outline">
+                            Discover It
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+// Export
+export default Home;
